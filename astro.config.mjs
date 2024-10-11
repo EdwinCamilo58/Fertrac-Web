9ed060660a175f851101f158207de1526917882b
edwincamilo58 <edwincamilo@live.com>
2024-10-10 22:53:01 -0500

    verce

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 
 
 // https://astro.build/config
